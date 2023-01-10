cf922556cab6dd4eec432c384a56a6e828615a6a
Vaibhav <109816361+Vaibhav-v-m@users.noreply.github.com>
2023-01-10 20:02:00 +0530

    Add files via upload

---
+++ EMS/src/main/java/com/greatlearning/ems/conroller/EmployeeController.java
@@ -0,0 +1,89 @@
+package com.greatlearning.ems.conroller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.greatlearning.ems.entity.Employee;
+import com.greatlearning.ems.service.EmployeeService;
+
+@Controller
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
+	@GetMapping("/employees")
+	public String listEmployees(Model model) {
+
+		List<Employee> employees = employeeService.listEmployees();
+
+		model.addAttribute("employees", employees);
+		return "employees";
+
+	}
+
+//	for adding employee
+
+	@GetMapping("/employees/new")
+	public String addEmployeeButtonClicked(Model model) {
+
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "create_employee";
+
+	}
+
+	@PostMapping("/employees")
+	public String submitButtonClickedForAddEmployee(@ModelAttribute("employee") Employee employee) {
+
+		employeeService.saveEmployee(employee);
+		return "redirect:/employees";
+
+	}
+
+//	for updating employee
+
+	@GetMapping("/employees/edit/{id}")
+	public String updateEmployeeButtonClicked(@PathVariable Long id, Model model) {
+
+		Employee selectedEmployee = employeeService.getEmployeeById(id);
+		model.addAttribute("employee", selectedEmployee);
+
+		return "edit_employee";
+
+	}
+
+	@PostMapping("/employees/{id}")
+	public String submitButtonClickedForUpdateEmployee(@PathVariable Long id,
+			@ModelAttribute("employee") Employee employee) {
+
+		Employee existingEmployeeObj = employeeService.getEmployeeById(id);
+		existingEmployeeObj.setFirstName(employee.getFirstName());
+		existingEmployeeObj.setFirstName(employee.getLastName());
+		existingEmployeeObj.setFirstName(employee.getEmail());
+
+		employeeService.updateEmployee(employee);
+		return "redirect:/employees";
+	}
+
+	// Update Employee - End
+
+//	for deleting employee
+
+	@GetMapping("/employees/{id}")
+	public String deleteEmployee(@PathVariable Long id) {
+
+		employeeService.deleteById(id);
+		return "redirect:/employees";
+	}
+
+}
